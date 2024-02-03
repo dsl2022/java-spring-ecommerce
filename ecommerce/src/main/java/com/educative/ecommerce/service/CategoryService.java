32dece18fd76ec0d4149e0b0d81f1584f2be9e22
dsl2022 <2015rpro@gmail.com>
2024-02-03 17:49:59 -0500

    added controller repository model for category

---
+++ ecommerce/src/main/java/com/educative/ecommerce/service/CategoryService.java
@@ -0,0 +1,12 @@
+package com.educative.ecommerce.service;
+
+
+import com.educative.ecommerce.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryService {
+    @Autowired
+    private CategoryRepository categoryRepository;
+}
